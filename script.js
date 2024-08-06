fed7a832659805d53dd307def08ba10b00461d9d
Sanket Kachare <sanketsk74@gmail.com>
2024-08-06 22:22:46 +0530

    added new functionality to open modal and added function namely openModal()

--- script.js
+++ script.js
@@ -10,22 +10,49 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+// this is old code to open the modal
 
-for (let i = 0 ; i < btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click',function(){
-        console.log('Button Clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
+// for (let i = 0 ; i < btnsOpenModal.length; i++){
+//     btnsOpenModal[i].addEventListener('click',function(){
+//         console.log('Button Clicked');
+//         modal.classList.remove('hidden');
+//         overlay.classList.remove('hidden');
 
-        modal.style.display = 'block';
-    })
+//         modal.style.display = 'block';
+//     })
+// }
+
+
+
+// updated code to open the modal
+
+// declaring the function for openModal
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    modal.style.display = 'block';
 }
+
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+   
+    //passing the openModal() to event listner
+    
+    btnsOpenModal[i].addEventListener('click',openModal);
+}
+
+
+// old code to close the modal
 
 // btnCloseModal.addEventListener('click',function(){
 //     modal.classList.add('hidden');
 //     overlay.classList.add('hidden');
 //     modal.style.display = 'none ';
 // }) 
+
+
+// updated code to close the modal
 
 // here we create a closeModal function 
 
